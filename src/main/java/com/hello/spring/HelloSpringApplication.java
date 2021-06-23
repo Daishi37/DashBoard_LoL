0937cec40775a0ab8b13e19864eb753a8c4b2693
argnd <gandour37@gmail.com>
2021-06-23 11:29:11 +0200

    Update HelloSpringApplication.java

--- src/main/java/com/hello/spring/HelloSpringApplication.java
+++ src/main/java/com/hello/spring/HelloSpringApplication.java
@@ -21,7 +21,7 @@ public class HelloSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        p.populate();
-        p.populate();
+//        p.populate();
+//        p.populate();
     }
 }
